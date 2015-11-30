4ee600a905966ba73357641a836c24a91f33b801
Nick Tursky <mturs@softserveinc.com>
2015-11-30 23:40:00 +0200

    Changed Amazon API Gateway importer to be a git submodule

--- aws-apigateway-importer/aws-api-import.cmd
+++
@@ -1,1 +0,0 @@
-java -jar target/aws-apigateway-importer-1.0.3-SNAPSHOT-jar-with-dependencies.jar  %*
--- aws-apigateway-importer/aws-api-import.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-java -jar target/aws-apigateway-importer-1.0.3-SNAPSHOT-jar-with-dependencies.jar "$@"
