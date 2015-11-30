2b6daf918a4d4e3b0f2323e3b3db09ffbdde89ab
Nick Tursky <mturs@softserveinc.com>
2015-11-30 21:08:25 +0200

    Amazon API Gateway Importer tool

---
+++ aws-apigateway-importer/aws-api-import.cmd
@@ -0,0 +1,1 @@
+java -jar target/aws-apigateway-importer-1.0.3-SNAPSHOT-jar-with-dependencies.jar  %*
---
+++ aws-apigateway-importer/aws-api-import.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -jar target/aws-apigateway-importer-1.0.3-SNAPSHOT-jar-with-dependencies.jar "$@"
